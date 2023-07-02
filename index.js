bad61f4db25ad8a1764d9659435f7a04d4cfd8d5
Nyakamba <omwegaenock@gmail.com>
2023-07-02 13:32:02 +0300

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-const add = require("./add"); //the add const an be anything
-
-console.log("Hello from index.js");
-const sum = add(1, 2);
-const sum2 = add(2, 3);
-console.log(sum);
-
-console.log(sum2);
+require("./batman");
+require("./superman");
