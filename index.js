abfad93608cf269c1b79cdc57d94cbd60c0eef8e
Nyakamba <omwegaenock@gmail.com>
2023-07-03 19:58:41 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-require("./batman");
-require("./superman");
+const superHero = require("./super-hero");
+console.log(superHero.getName());
+superHero.setName("Superman");
+console.log(superHero.getName());
