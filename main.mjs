edf94e123ee0402bf890a6eeca4f906605d5195e
Nyakamba <omwegaenock@gmail.com>
2023-07-04 16:19:00 +0300

    Es modules

--- main.mjs
+++ main.mjs
@@ -1,6 +1,6 @@
-import math from "./math-esm.mjs";
+import { add, subtract } from "./math-esm.mjs";
 
 const { add, subtract } = math;
 
-console.log(math.add(5, 5));
-console.log(math.subtract(5, 5));
+console.log(add(5, 5));
+console.log(subtract(5, 5));
