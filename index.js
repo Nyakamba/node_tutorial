300bd20236c3501adb0795a2e9551637f8ff1706
Nyakamba <omwegaenock@gmail.com>
2023-07-06 10:33:59 +0300

    Events module

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const EventEmitter = require("node:events");
+
+const emitter = new EventEmitter();
+
+emitter.on("order-pizza", (size, topping) => {
+  console.log(`Order received! Baking a ${size} pizza with ${topping}`);
+});
+
+emitter.on("order-pizza", (size) => {
+  if (size === "large") {
+    console.log("Serving complimentary drink");
+  }
+});
+
+emitter.emit("order-pizza", "large", "mushroom");
