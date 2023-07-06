cbe009c20b5a4f353fe55ccd2abac997e9ff491a
Nyakamba <omwegaenock@gmail.com>
2023-07-06 09:01:52 +0300

    Events module

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-function green(name) {
-  console.log(`Hello ${name}`);
-}
-
-function higherOrderFunction(callback) {
-  const name = "Vishwas";
-  callback(name);
-}
-
-higherOrderFunction(green);
+const EventEmitter = require("node:events");
