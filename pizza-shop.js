e55defe03a6d785578cbdb4eb253db70a0218c0f
Nyakamba <omwegaenock@gmail.com>
2023-07-08 22:08:47 +0300

    Event emitter

--- pizza-shop.js
+++ pizza-shop.js
@@ -1,14 +1,18 @@
-class PizzaShop {
+const EventEmitter = require("node:events");
+
+class PizzaShop extends EventEmitter {
   constructor() {
+    super();
     this.orderNumber = 0;
   }
 
-  order() {
+  order(size, topping) {
     this.orderNumber++;
+    this.emit("order", size, topping);
   }
 
   displayOrderNumber() {
-    console.log(`Current order number ${this.orderNumber}`);
+    console.log(`Current order number: ${this.orderNumber}`);
   }
 }
 
