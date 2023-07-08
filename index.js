3df5cda36824de11ed4186184fadd36c201de0d1
Nyakamba <omwegaenock@gmail.com>
2023-07-08 23:42:28 +0300

    Event emitter

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const PizzaShop = require("./pizza-shop");
+const DrinkMachine = require("./drink-machine");
 
 const pizzaShop = new PizzaShop();
+const drinkMachine = new DrinkMachine();
 
 pizzaShop.on("order", (size, topping) => {
   console.log(`Order received! Baking a ${size} pizza with ${topping}`);
+  drinkMachine.serveDrink(size);
 });
 
 pizzaShop.order("large", "mushrooms");
