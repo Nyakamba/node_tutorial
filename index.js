ef1d5d994f3f34cbf6b38efe735c82b30732d139
Nyakamba <omwegaenock@gmail.com>
2023-07-11 15:54:46 +0300

    fs module

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-const buffer = new Buffer.from("Vishwas");
-
-buffer.write("Code");
-
-console.log(buffer.toJSON());
-console.log(buffer);
-console.log(buffer.toString());
+const fs = require("node:fs");
