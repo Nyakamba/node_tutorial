34f03cf18de2037f03fdf4304b86939506d003fd
Nyakamba <omwegaenock@gmail.com>
2023-07-11 15:30:28 +0300

    buffers and streams

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-const PizzaShop = require("./pizza-shop");
-const DrinkMachine = require("./drink-machine");
+const buffer = new Buffer.from("Vishwas");
 
-const pizzaShop = new PizzaShop();
-const drinkMachine = new DrinkMachine();
-
-pizzaShop.on("order", (size, topping) => {
-  console.log(`Order received! Baking a ${size} pizza with ${topping}`);
-  drinkMachine.serveDrink(size);
-});
-
-pizzaShop.order("large", "mushrooms");
-pizzaShop.displayOrderNumber();
-
-// const EventEmitter = require("node:events");
-
-// const emitter = new EventEmitter();
-
-// emitter.on("order-pizza", (size, topping) => {
-//   console.log(`Order received! Baking a ${size} pizza with ${topping}`);
-// });
-
-// emitter.on("order-pizza", (size) => {
-//   if (size === "large") {
-//     console.log("Serving complimentary drink");
-//   }
-// });
-
-// emitter.emit("order-pizza", "large", "mushroom");
+console.log(buffer.toJSON());
+console.log(buffer);
