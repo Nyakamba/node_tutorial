7fdd769aa78bd807ab6f975e97a0d8406c137649
Nyakamba <omwegaenock@gmail.com>
2023-07-11 16:12:35 +0300

    Callback function in fs module

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+const { error } = require("node:console");
 const fs = require("node:fs");
 
+console.log("First");
 const fileContents = fs.readFileSync("./file.txt", "utf-8");
 console.log(fileContents);
+
+console.log("Second");
+
+fs.readFile("./file.txt", "utf-8", (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
+
+console.log("Third");
