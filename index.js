d9dc22799af81e3b707d784c869449083043765d
Nyakamba <omwegaenock@gmail.com>
2023-07-12 15:46:20 +0300

    pipes

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const fs = require("node:fs");
+const zlib = require("node:zlib");
+
+const gzip = zlib.createGzip();
 
 const readableStream = fs.createReadStream("./file.txt", {
   encoding: "utf-8",
   highWaterMark: 2,
 });
+
+readableStream.pipe(gzip).pipe(fs.WriteStream("./file2.txt.gz"));
 
 const writeableStream = fs.createWriteStream("./file2.txt");
 
