49f3cb8c145fdcbc1d022f0cc401fc1a940a8c01
Nyakamba <omwegaenock@gmail.com>
2023-07-23 21:31:12 +0300

    Html response

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
     firstName: "Bruce",
     lastName: "Wayne",
   };
-  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/html" });
   res.end("<h1>Hello world</h1>");
 });
 
