e42ad6dcaeeded8d7da6ec8b91824a028cd40792
Nyakamba <omwegaenock@gmail.com>
2023-07-23 21:43:04 +0300

    html response

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const http = require("node:http");
+const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  const superHero = {
-    firstName: "Bruce",
-    lastName: "Wayne",
-  };
+  //   const superHero = {
+  //     firstName: "Bruce",
+  //     lastName: "Wayne",
+  //   };
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello world</h1>");
+  fs.createReadStream(__dirname + "/index.html").pipe(res);
+  //   const html = fs.readFileSync("index.html", "utf-8");
+  //   res.end(html);
 });
 
 server.listen(3000, () => {
