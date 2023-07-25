58131c8bb7c38889837c60054fcb544cf4166c66
Nyakamba <omwegaenock@gmail.com>
2023-07-25 22:51:15 +0300

    html templates

--- index.js
+++ index.js
@@ -2,14 +2,7 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  //   const superHero = {
-  //     firstName: "Bruce",
-  //     lastName: "Wayne",
-  //   };
-  res.writeHead(200, { "Content-Type": "text/html" });
-  fs.createReadStream(__dirname + "/index.html").pipe(res);
-  //   const html = fs.readFileSync("index.html", "utf-8");
-  //   res.end(html);
+  const name = "Vishwas";
 });
 
 server.listen(3000, () => {
