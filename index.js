8e563c82f5a7a8097f03d769558b7e81254eb902
Nyakamba <omwegaenock@gmail.com>
2023-07-26 09:54:00 +0300

    html template

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(
       JSON.stringify({
-        firstName: "Bruce",
+        firstName: "Brucee",
         lastName: "Waynee",
       })
     );
