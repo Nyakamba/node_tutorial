ceda845f8b78e366a95f5fca260f8b2252e84fe9
Nyakamba <omwegaenock@gmail.com>
2023-07-26 09:53:40 +0300

    html template

--- index.js
+++ index.js
@@ -2,7 +2,24 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  const name = "Vishwas";
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Home page");
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("About page");
+  } else if (req.url === "/api") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        firstName: "Bruce",
+        lastName: "Waynee",
+      })
+    );
+  } else {
+    res.writeHead(404);
+    res.end("Page not found");
+  }
 });
 
 server.listen(3000, () => {
