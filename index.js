58f878b88166eb4929d37f59e650f76ac14902cd
Nyakamba <omwegaenock@gmail.com>
2023-08-16 15:16:30 +0300

    asynch

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const crypto = require("node:crypto");
 
+const MAX_CALLS = 1;
+
 const start = Date.now();
-crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
-crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
-crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
-console.log("Hash: ", Date.now() - start);
+
+for (let i = 0; i < MAX_CALLS; i++) {
+  crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
+    console.log("Hash: ${i+1}", Date.now() - start);
+  });
+}
