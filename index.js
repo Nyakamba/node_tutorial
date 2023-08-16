c9266ce2077358113c95e9ed43f468e8fafcacc7
Nyakamba <omwegaenock@gmail.com>
2023-08-16 15:20:05 +0300

    asynch libuv v

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const crypto = require("node:crypto");
 
-const MAX_CALLS = 1;
+const MAX_CALLS = 3;
 
 const start = Date.now();
 
 for (let i = 0; i < MAX_CALLS; i++) {
   crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
-    console.log("Hash: ${i+1}", Date.now() - start);
+    console.log(`Hash: ${i + 1}`, Date.now() - start);
   });
 }
