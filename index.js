c19f832e3e2a32bdb33d916c964faa53d4de3115
Nyakamba <omwegaenock@gmail.com>
2023-08-16 15:00:17 +0300

    libuv

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
-const http = require("node:http");
 const fs = require("node:fs");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Home page");
-  } else if (req.url === "/about") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("About page");
-  } else if (req.url === "/api") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        firstName: "Brucee",
-        lastName: "Waynee",
-      })
-    );
-  } else {
-    res.writeHead(404);
-    res.end("Page not found");
-  }
+console.log("First");
+
+fs.readFile("./file.txt", "utf-8", (err, data) => {
+  console.log("First contents");
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+console.log("Last");
+
+// const http = require("node:http");
+// const fs = require("node:fs");
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     res.writeHead(200, { "Content-Type": "text/plain" });
+//     res.end("Home page");
+//   } else if (req.url === "/about") {
+//     res.writeHead(200, { "Content-Type": "text/plain" });
+//     res.end("About page");
+//   } else if (req.url === "/api") {
+//     res.writeHead(200, { "Content-Type": "application/json" });
+//     res.end(
+//       JSON.stringify({
+//         firstName: "Brucee",
+//         lastName: "Waynee",
+//       })
+//     );
+//   } else {
+//     res.writeHead(404);
+//     res.end("Page not found");
+//   }
+// });
+
+// server.listen(3000, () => {
+//   console.log("Server running on port 3000");
+// });
 
 // const fs = require("node:fs");
 // const zlib = require("node:zlib");
