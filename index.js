e3f6db5a17ce46a6c1e83e585af73a32deb6080e
Nyakamba <omwegaenock@gmail.com>
2023-08-16 15:10:52 +0300

    sync in main thread

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const crypto = require("node:crypto");
+
 const start = Date.now();
+crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
 crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
 console.log("Hash: ", Date.now() - start);
