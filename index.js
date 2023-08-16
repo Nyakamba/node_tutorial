ec1bcc669b72506c277dd6116c4dbfa2f8e62da7
Nyakamba <omwegaenock@gmail.com>
2023-08-16 22:36:52 +0300

    network I/O

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const crypto = require("node:crypto");
-process.env.UV_THREADPOOL_SIZE = 16;
+const https = require("node:https");
+// const crypto = require("node:crypto");
+
+// process.env.UV_THREADPOOL_SIZE = 16;
+
 const MAX_CALLS = 16;
 
 const start = Date.now();
 
 for (let i = 0; i < MAX_CALLS; i++) {
-  crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
-    console.log(`Hash: ${i + 1}`, Date.now() - start);
-  });
+  https
+    .request("https://www.google.com", (res) => {
+      res.on("data", () => {});
+      res.on("end", () => {
+        console.log(`Request:${i + 1}`, Date.now() - start);
+      });
+    })
+    .end();
+
+  // crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
+  //   console.log(`Hash: ${i + 1}`, Date.now() - start);
+  // });
 }
